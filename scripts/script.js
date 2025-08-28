9f64491ac5e5d4bc184d5355d93925c2fde2587d
khandakershahi <khandakershahi@gmail.com>
2025-08-29 01:09:28 +0600

    Challenge task done

--- scripts/script.js
+++ scripts/script.js
@@ -16,30 +16,76 @@ heart();
 
 
 // call function
-function callService(id) {
-    const serviceName = document.getElementById('service-name-national').innerText;
-    const serviceNumber = document.getElementById('service-number-national').innerText;
+function callService(btnId, nameId, numberID) {
+    const serviceName = document.getElementById(nameId).innerText;
+    const serviceNumber = document.getElementById(numberID).innerText;
 
-    document.getElementById(id).addEventListener('click', function () {
-        alert(`calling ${serviceName} ${serviceNumber}...`)
+    document.getElementById(btnId).addEventListener('click', function () {
+        let coinCountNumber = parseInt(document.getElementById('coin-count').innerText);
+        if (coinCountNumber >=  20) {
+
+            coinCountNumber = coinCountNumber - 20;
+            document.getElementById('coin-count').innerText = coinCountNumber;
+            alert(`calling ${serviceName} ${serviceNumber}...`);
+
+            const d = new Date();
+            let time = d.toLocaleTimeString();
+            const historyData = document.getElementById('history-body-wrapper');
+            const newCallData = `
+         <div class="history-body bg-[#FAFAFA] flex justify-between gap-4 items-center p-4 rounded-[8px] mb-2">
+                <div class="left">
+                    <h3 class="history-card-title">${serviceName}</h3>
+                    <p class="card-category">${serviceNumber}</p>
+                </div>
+                <div class="right">
+                  <p class="card-category">${time}</p>
+                </div>
+            </div>
+         `;
+            historyData.innerHTML += newCallData;
+
+        } else {
+            alert(`You don't have enough coin`);
+        }
     })
 
 }
 
-callService('call-national');
+callService('call-national','service-name-national','service-number-national');
+callService('call-police', 'service-name-police','service-number-police')
+
+// history clear button
+function historyClear() {
+    document.getElementById('clear').addEventListener('click', function (event) {
+        const historyData = document.getElementById('history-body-wrapper');
+        historyData.remove(historyData.childNodes);
+    })
+
+}
+
+historyClear();
 
 // copy function
-function copyNumberNational() {
-    let copyNumber = document.getElementById('service-number-national');
-     navigator.clipboard.writeText(copyNumber.innerText);
+function copyNumber(btnId, serviceNumberId) {
+    let copyNumber = document.getElementById(serviceNumberId);
 
-    document.getElementById('copy-number-national').addEventListener('click', function () {
-        
+
+    document.getElementById(btnId).addEventListener('click', function () {
+
+        navigator.clipboard.writeText(copyNumber.innerText);
+
         alert("Number copied: " + copyNumber.innerText);
+        const copyCounter =document.getElementById('copy-counter');
+        let copyCountNumber = parseInt(copyCounter.innerText);
+       copyCountNumber+= 1;
+       copyCounter.innerText = copyCountNumber;
+       
     })
 
 }
-copyNumberNational()
+copyNumber('copy-number-national','service-number-national')
+copyNumber('copy-number-police','service-number-police')
+
 
 
 
