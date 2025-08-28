d3477a7eaf6e75ae09e4982c62e1c8c50767c19d
khandakershahi <khandakershahi@gmail.com>
2025-08-28 19:22:47 +0600

    function coding started

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+// heart count function;
+function heart() {
+    const hearts = document.querySelectorAll('.heart-click');
+    const countHeart = document.getElementById('heart-count');
+    let count = parseInt(countHeart.innerText);
+
+    for (const heart of hearts) {
+        heart.addEventListener('click', function () {
+            count += 1;
+            countHeart.innerText = count;
+        });
+    }
+}
+
+heart();
+
+
+// call function
+function callService(id) {
+    const serviceName = document.getElementById('service-name').innerText;
+    const serviceNumber = document.getElementById('service-number').innerText;
+
+    document.getElementById(id).addEventListener('click', function () {
+        alert(`calling ${serviceName} ${serviceNumber}...`)
+    })
+
+}
+
+callService('call-national');
+
+// copy function
+function copyNumber(id) {
+    let copyNumber = document.getElementById('service-number');
+
+    navigator.clipboard.writeText(copyNumber.innerText);
+
+    document.getElementById(id).addEventListener('click', function () {
+        alert("Number copied: " + copyNumber.innerText);
+    })
+
+
+}
+
+copyNumber('copy-number-national');
+
